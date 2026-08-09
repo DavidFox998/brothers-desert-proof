ec194331aab9d42c8ef2fff6f3dc81918cf707c9
DAVID J FOX <davidjfox998@gmail.com>
2026-08-08 21:39:32 -0700

    Update lakefile.lean with new package requirements

--- lakefile.lean
+++ lakefile.lean
@@ -1,3 +1,20 @@
+import Lake
+open Lake DSL
+
+package «brothers-desert-proof» where
+  version := v!"0.1.0"
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.15.0"
+
+require «eutheos-property» from git
+  "https://github.com/DavidFox998/eutheos-property.git" @ "main"
+
+require «lindelof-hypothesis-143» from git
+  "https://github.com/DavidFox998/lindelof-hypothesis-143.git" @ "main"
+
+lean_lib BrothersDesertProof where
+  srcDir := "."
   globs := #[
     .one `Family.Brothers1419,
     .one `Family.TwinPrimes,
